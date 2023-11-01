90cdd51a98d0a8a22e9143dd020ea88a76e5e8ff
Mario Rodriguez Martinez <mariog.rodriguezm@gmail.com>
2023-11-01 11:06:39 +0100

    initial commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import { NavLink } from "react-router-dom";
+
+const Navbar = () => {
+  return( 
+  <div>
+  <NavLink to="/">Inicio</NavLink>
+  <NavLink to="/login">Login</NavLink>
+
+    
+  </div>
+  )
+};
+export default Navbar;
---
+++ src/context/UserProvider.jsx
@@ -0,0 +1,14 @@
+import { createContext, useState } from "react"
+
+ 
+ export const userContext = createContext()
+
+ const UserProvider = (props) => {
+    const [user, setUser] = useState(false)
+  return (
+   <userContext.Provider value={{user, setUser}}>
+{props.children}
+   </userContext.Provider>
+  )
+}
+export default UserProvider
